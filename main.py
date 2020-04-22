946cdfe5a0da131f7bd631121bbac4edd3a0b485
Ananthakrishnan Narayanan <55069729+ananth-narayanan@users.noreply.github.com>
2020-04-22 13:26:00 -0400

    Refectored code for better readability

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datasimulator import datasimulator
 from preparedata import *
 from chi2 import Chi2
 
-#Edit values below
+#Edit values below to experiment
 survival_sim=[0.25,0.50,0.75,0.85,0.95, 0.97] #Simulation survival rates
 random_seed = True # Set to False if you want to get repeatable results
 survival_test =[0.95, 0.97] # Two values for Chi2 and Bayesian AB test
@@ -14,13 +14,15 @@ batchsize=[12,12]
 priors=[[1,1]]*len(survival_test)
 
 
-# Do not edit code below if you are not familiar with python
-data_sim=datasimulator(survival_sim, random_seed)
+# Do not edit code below if you are not comfortable with python
+data_raw=datasimulator(survival_sim, random_seed)
+data_processed=preparedata(data_raw, survival_test)
 
-if len(survival_test)==2:
-    chi2_data =preparedata_chi2(data_sim, survival_test)
+#chi2 test written only for 2 factors
+if data_processed.shape[1]==2:
     chi2_batchsize=dict(zip(survival_test, batchsize))
-    Chi2(chi2_data, chi2_batchsize).calculate()
+    Chi2(data_processed, chi2_batchsize).calculate()
 
-bayesian_data, beta_params=preparedata_bayes(data_sim, survival_test, priors)
-Bayesian_ab_beta(bayesian_data, beta_params).calculate()
+#Bayesian test written only for more than 2 factors
+beta_params=dict(zip(survival_test, priors))
+Bayesian_ab_beta(data_processed, beta_params).calculate()
