31bce79828c2730791043ba17d54f02c398d332e
Ananthakrishnan Narayanan <55069729+ananth-narayanan@users.noreply.github.com>
2020-04-22 13:26:29 -0400

    Refactored code for better readability

--- preparedata.py
+++ preparedata.py
@@ -16,25 +16,3 @@ def preparedata(df, survival_rate):
             raise ValueError('Data not available for one of more survival rates')
     except ValueError as ve:
             print(ve)
-
-
-
-def preparedata_bayes(df, survival_rate, priors):
-    try:
-        bayesian_data= preparedata(df, survival_rate)
-        beta_params=dict(zip(survival_rate, bayesian_data.shape[1]*priors))
-        return bayesian_data, beta_params
-        if len(survival_rate)!= len(priors):
-            raise ValueError('Lengths of survival_rate and batchsize must be =2')
-    except ValueError as ve:
-        print(ve)
-
-
-def preparedata_chi2(df, survival_rate):
-    try:
-        chi2_data= preparedata(df, survival_rate)
-        return chi2_data
-        if len(survival_rate)!=2 or len(batchsize)!=2:
-            raise ValueError('Lengths of survival_rate and batchsize must be =2')
-    except ValueError as ve:
-        print(ve)
